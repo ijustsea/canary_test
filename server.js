b880fffccd200c7ee973ce4579af6e8019fea582
ijustsea <wlsqkd1199@naver.com>
2026-03-31 14:39:17 +0900

    second commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 8080;
 const COLOR = process.env.COLOR || 'blue';
 const VERSION = 'V1';
 
+
 http.createServer((req, res)=>{
     res.writeHead(200, {'Content-type': 'text/plain; charset=utf-8'});
     res.end(`[${VERSION}] 현재응답 서버식별: ${COLOR} (포트: ${PORT})\n`);
